d21948b8570efd118306f4bf552af7d86aed4d38
Brian Anashari <brianashari18@gmail.com>
2024-12-05 15:03:52 +0700

    modify homepage

--- src/components/HowItWorks.jsx
+++ src/components/HowItWorks.jsx
@@ -1,21 +1,17 @@
+import logoImage from '../assets/white-logo.png';
+
 const HowItWorks = () => {
     return (
-        <section className="p-10 bg-blue-500 text-white">
-            <h2 className="text-center text-2xl font-bold mb-6">How It Works?</h2>
-            <div className="grid md:grid-cols-3 gap-6">
-                <div className="text-center">
-                    <h3 className="font-bold text-lg mb-2">1. Find Venues</h3>
-                    <p>Easily find sports venues near you.</p>
-                </div>
-                <div className="text-center">
-                    <h3 className="font-bold text-lg mb-2">2. Real-time Booking</h3>
-                    <p>Check availability in real-time.</p>
-                </div>
-                <div className="text-center">
-                    <h3 className="font-bold text-lg mb-2">3. Play and Enjoy</h3>
-                    <p>Book and enjoy with your friends.</p>
+        <section className="mx-60 my-20">
+            <div className="container relative h-[40rem] w-full mb-48 rounded-3xl shadow-inner-dark">
+                <div className="absolute top-10 left-10 right-0 bottom-0 h-[45rem] w-full p-16 bg-blue-500 text-[#f5f5f5] rounded-3xl shadow-3xl">
+                    <div className="flex items-center justify-between">
+                        <div className="font-poppins font-extrabold text-6xl">How it works?</div>
+                        <img src={logoImage} alt="logo"/>
+                    </div>
                 </div>
             </div>
+
         </section>
     );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,14 @@ export default {
       },
       colors: {
         'primary': '#738FFD',
-      }
+      },
+      boxShadow: {
+        'inner-dark': 'inset 5px 5px 15px 10px rgba(0, 0, 0, 0.15)',
+        '3xl': ' 5px 5px 15px 10px rgba(0, 0, 0, 0.15)',
+      },
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+      },
     },
   },
   plugins: [],
